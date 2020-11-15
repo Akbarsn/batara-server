682e8bfee8a7df6c1b2ffe5384d98bd01f1eeda2
Akbarsn <personal.akbarsn@gmail.com>
2020-11-15 08:09:08 +0700

    Add dotenv

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
+require('dotenv').config()
+
 const express = require('express');
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.get('/ping', (req, res) => {
     res.json({
